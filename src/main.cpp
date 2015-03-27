bc95a707d23dcbb731d559a8f0c0a0d5c6b0cdbe
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2015-03-26 21:58:46 -0700

    Optimize box rendering
    Submit just one batch with all boxes at once.

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@ sf::Vector2f ConvertVector(Vector2f vec)
   return sf::Vector2f(vec.x, vec.y);
 }
 
-void RenderBox(sf::RenderWindow *window, Coords2f coords, Vector2f size, sf::Color color)
+void RenderBox(std::vector<sf::Vertex>& vertices, Coords2f coords, Vector2f size, sf::Color color)
 {
   Vector2f localPoints[4];
   localPoints[0] = Vector2f(-size.x, -size.y);
@@ -19,14 +19,13 @@ void RenderBox(sf::RenderWindow *window, Coords2f coords, Vector2f size, sf::Col
   localPoints[2] = Vector2f(size.x, size.y);
   localPoints[3] = Vector2f(-size.x, size.y);
 
-  sf::Vertex vertices[4];
   for (int vertexNumber = 0; vertexNumber < 4; vertexNumber++)
   {
-    vertices[vertexNumber].color = color;
-    vertices[vertexNumber].position = ConvertVector(coords.GetPointGlobalPos(localPoints[vertexNumber]));
+    sf::Vertex v;
+    v.color = color;
+    v.position = ConvertVector(coords.GetPointGlobalPos(localPoints[vertexNumber]));
+    vertices.push_back(v);
   }
-
-  window->draw(vertices, 4, sf::Quads);
 }
 
 float random(float min, float max)
@@ -120,6 +119,8 @@ int main(int argc, char** argv)
 
   bool paused = 0;
 
+  std::vector<sf::Vertex> vertices;
+
   while (window->isOpen())
   {
     sf::Event event;
@@ -136,6 +137,7 @@ int main(int argc, char** argv)
       paused = 0;
     }
     window->clear(sf::Color(50, 50, 50, 255));
+    vertices.clear();
     if (clock.getElapsedTime().asSeconds() > prevUpdateTime + integrationTime)
     {
       sf::Clock physicsClock;
@@ -180,7 +182,7 @@ int main(int argc, char** argv)
         color = sf::Color(242, 236, 164, 255);
       }
 
-      RenderBox(window, bodyCoords, size, color);
+      RenderBox(vertices, bodyCoords, size, color);
     }
 
 
@@ -205,7 +207,7 @@ int main(int argc, char** argv)
           redMult = 0.5f;
 
         sf::Color color1(100, char(100 * redMult), char(100 * redMult), 100);
-        RenderBox(window, coords, Vector2f(3.0f, 3.0f), color1);
+        RenderBox(vertices, coords, Vector2f(3.0f, 3.0f), color1);
 
         coords.pos = man.body2->coords.pos + man.collisions[collisionNumber].delta2;
         sf::Color color2(150, char(150 * redMult), char(150 * redMult), 100);
@@ -213,9 +215,12 @@ int main(int argc, char** argv)
         //{
         //  color2 = sf::Color(255, 0, 0, 255);
         //}
-        RenderBox(window, coords, Vector2f(3.0f, 3.0f), color2);
+        RenderBox(vertices, coords, Vector2f(3.0f, 3.0f), color2);
       }
     }
+
+    if (vertices.size() > 0)
+      window->draw(&vertices[0], vertices.size(), sf::Quads);
 
     std::stringstream debugTextStream;
     debugTextStream << "Bodies count: " << physSystem.GetBodiesCount() << " contacts count: " << physSystem.GetJointsCount();
